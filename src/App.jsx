e47e4384a040bc910ca25b747d73c601bdcfdff5
Dali Duraku <mesherbetmolla@gmail.com>
2025-02-12 13:31:39 -0500

    feat: accessibility improvement
    - the new game button will be focused when the game is won

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useRef, useEffect } from 'react'
 import Die from "./Die"
 import { nanoid } from "nanoid"
 import Confetti from "react-confetti"
@@ -6,8 +6,16 @@ import Confetti from "react-confetti"
 export default function App() {
   const [dice, setDice] = useState(() => generateAllNewDice())
 
+  const buttonRef = useRef(null)
+
   const gameWon = dice.every(die => die.isHeld) &&
     dice.every(die => die.value === dice[0].value)
+
+  useEffect(() => {
+    if(gameWon){
+      buttonRef.current.focus()
+    }
+  }, [gameWon])
 
   function generateAllNewDice() {
     return new Array(10)
@@ -62,7 +70,7 @@ export default function App() {
       <div className="dice-container">
         {diceElements}
       </div>
-      <button className="roll-dice" onClick={rollDice}>{gameWon ? "New Game": "Roll"}</button>
+      <button ref={buttonRef} className="roll-dice" onClick={rollDice}>{gameWon ? "New Game": "Roll"}</button>
     </main>
   )
 }
